cb8a0c740965528c4ee07657f89b73c94f2e6791
duck-dev781 <elletsonchase@gmail.com>
2026-09-21 17:06:21 -0500

    Seed ESP-Bedrock project structure: include/espbedrock_config.h

---
+++ include/espbedrock_config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define ESPBEDROCK_VERSION "0.1.0"
+#define ESPBEDROCK_UDP_PORT 19132
+#define ESPBEDROCK_MAX_PLAYERS 4
+
+#define ESPBEDROCK_WORLD_DIR "/espbedrock/world"
+#define ESPBEDROCK_CONFIG_DIR "/espbedrock/config"
+#define ESPBEDROCK_ASSET_DIR "/espbedrock/assets"
+
+#define ESPBEDROCK_WORLD_FILE "/espbedrock/world/world.dat"
+#define ESPBEDROCK_PLAYER_FILE "/espbedrock/world/players.dat"
+
+#define ESPBEDROCK_CHUNK_SIZE 16
+#define ESPBEDROCK_WORLD_HEIGHT 64
